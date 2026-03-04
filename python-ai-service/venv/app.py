6a94b29da16b76c7af3c39cb2a7883afd2acc0f6
Fragger99 <99fragger99@gmail.com>
2026-03-04 11:31:41 +0530

    AI processing is done

--- python-ai-service/venv/app.py
+++ python-ai-service/venv/app.py
@@ -1,3 +1,7 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
 from fastapi import FastAPI, File, UploadFile
 import shutil
 import os
@@ -20,15 +24,57 @@ async def analyze_image(image: UploadFile = File(...)):
         # Save uploaded file temporarily
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(image.file, buffer)
-
-        print(f"Image received: {file_path}")
+        image.file.close()
+        print(f"[AI SERVICE] Image received: {file_path}")
 
         # Simulate AI processing delay
-        time.sleep(2)
+        # time.sleep(2)
 
-        # Fake AI response
-        description = "This is a mock AI-generated description of the uploaded image."
+        # AI Processing
+        img = cv2.imread(file_path)
 
+        if img is None:
+            raise Exception("Invalid image file")
+
+        results = model(img)
+
+        person_count = 0
+
+        for result in results:
+            for box in result.boxes:
+                cls = int(box.cls[0])
+                conf = float(box.conf[0])
+
+                if cls == 0 and conf > 0.5:  # person with confidence filter
+                    person_count += 1
+
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+                    # draw rectangle
+                    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+
+                    # add label
+                    label = f"Person {conf:.2f}"
+                    cv2.putText(img, label, (x1, y1-10),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                0.5,
+                                (0,255,0),
+                                2)
+                    
+        output_path = os.path.join(UPLOAD_FOLDER, f"annotated_{int(time.time())}_{image.filename}")
+        cv2.imwrite(output_path, img)
+
+        print(f"[AI SERVICE] Annotated image saved: {output_path}")
+        
+        print(f"[AI SERVICE] Persons detected: {person_count}")
+        
+        if person_count == 0:
+            description = "No person detected in the image."
+        elif person_count == 1:
+            description = "1 person detected in the image."
+        else:
+            description = f"{person_count} persons detected in the image."
+        
         # Delete file after processing
         os.remove(file_path)
 
